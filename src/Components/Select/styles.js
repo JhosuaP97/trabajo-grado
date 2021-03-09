dc2c56b9f8c0065d1233dc4f0b38c10127ded159
Jhosua Pachon <jhosuapachon@gmail.com>
2021-03-08 23:14:39 -0500

    updated styles Select

--- src/Components/Select/styles.js
+++ src/Components/Select/styles.js
@@ -6,13 +6,36 @@ export const Dropdown = styled.div`
   min-width: 8rem;
   position: relative;
 
+  .formList {
+    display: block;
+    width: 10rem;
+    padding: 0.9rem 1rem;
+    border-radius: 0.5rem;
+    font-size: 14px;
+    outline: none;
+    line-height: 1rem;
+    border: 1px solid ${Colors.default};
+    cursor: pointer;
+    -moz-appearance: none; /* Firefox */
+    -webkit-appearance: none; /* Safari and Chrome */
+    appearance: none;
+
+    font-family: "Raleway";
+    font-style: normal;
+    font-weight: normal;
+    font-feature-settings: "pnum" on, "lnum" on;
+    option p {
+      padding: 1rem;
+    }
+  }
+
   &:after {
     content: "";
     position: absolute;
     right: 8px;
     top: 1rem;
-    width: 10px;
-    height: 10px;
+    width: 9px;
+    height: 9px;
     color: ${Colors.default};
     border-top: 2px solid;
     border-right: 2px solid;
@@ -25,60 +48,36 @@ export const Dropdown = styled.div`
   }
 
   label {
-    position: absolute;
-    left: 0.6rem;
-    color: ${Colors.default};
-    top: 0.95rem;
-    font-size: 14px;
-    transition: 0.3s;
     background-color: #fff;
+    color: ${Colors.default};
     font-family: "Raleway";
+    font-size: 0.875rem;
+    left: 0.6rem;
+    position: absolute;
+    top: 0.95rem;
+    transition: 0.3s;
+    user-select: none;
   }
 
-  select:focus,
-  select:valid {
+  .formList:focus + label {
+    color: ${Colors.focus};
+    font-size: 12px;
+    left: 0.3rem;
+    top: -0.5rem;
+    z-index: 10;
+  }
+
+  .formList:focus,
+  .formList:valid {
     border: 1px solid ${Colors.focus};
   }
 
-  select:focus + label,
-  select:valid + label {
+  .formList:focus + label,
+  .formList:valid + label {
     top: -0.5rem;
     left: 0.3rem;
     color: ${Colors.focus};
     font-size: 12px;
     z-index: 10;
-  }
-
-  select option {
-    background-color: transparent;
-    border-bottom: 1px solid ${Colors.default};
-    /* font-family: "Raleway";
-    font-style: normal; */
-  }
-`;
-
-export const OptionStyled = styled.option`
-  background-color: red;
-`;
-
-export const List = styled.select`
-  width: 10rem;
-  padding: 0.9rem 1rem;
-  border-radius: 0.5rem;
-  font-size: 14px;
-  outline: none;
-  line-height: 1rem;
-  border: 1px solid ${Colors.default};
-  cursor: pointer;
-  -moz-appearance: none; /* Firefox */
-  -webkit-appearance: none; /* Safari and Chrome */
-  appearance: none;
-
-  font-family: "Raleway";
-  font-style: normal;
-  font-weight: normal;
-  font-feature-settings: "pnum" on, "lnum" on;
-  option p {
-    padding: 1rem;
   }
 `;
