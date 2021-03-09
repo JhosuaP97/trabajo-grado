8438aa01c5175b49ddb3e2970019acb27016946d
Jhosua Pachon <jhosuapachon@gmail.com>
2021-03-08 23:11:45 -0500

    added PropTypes to RadioButton

--- src/Components/RadioButton/index.js
+++ src/Components/RadioButton/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { ContainerRadio, Radio } from "./styles";
+import PropTypes from "prop-types";
+
 const RadioButton = ({ id, text, name, value, onChange, checked }) => {
   return (
     <ContainerRadio>
@@ -16,6 +18,18 @@ const RadioButton = ({ id, text, name, value, onChange, checked }) => {
       <label htmlFor={id}>{text}</label>
     </ContainerRadio>
   );
+};
+
+RadioButton.propTypes = {
+  id: PropTypes.oneOfType([
+    PropTypes.string.isRequired,
+    PropTypes.number.isRequired,
+  ]),
+  text: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  value: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+  checked: PropTypes.bool.isRequired,
 };
 
 export default RadioButton;
