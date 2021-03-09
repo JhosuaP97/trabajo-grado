bb9d72ae97701b30c5d21515c04f3241316f4ba6
Jhosua Pachon <jhosuapachon@gmail.com>
2021-03-08 23:12:02 -0500

    added PropTypes to Select

--- src/Components/Select/index.js
+++ src/Components/Select/index.js
@@ -1,19 +1,35 @@
 import React from "react";
-import { Dropdown, List } from "./styles";
+import { Dropdown } from "./styles";
+import PropTypes from "prop-types";
+
 const Select = ({ options, onChange, name, placeholder, value }) => {
   return (
     <Dropdown>
-      <List name={name} onChange={onChange} value={value} required>
+      <select
+        className="formList"
+        name={name}
+        onChange={onChange}
+        value={value}
+        required
+      >
         <option value=""></option>
         {options.map((option) => (
           <option key={option.id} value={option.id}>
             {option.label}
           </option>
         ))}
-      </List>
+      </select>
       <label>{placeholder}</label>
     </Dropdown>
   );
+};
+
+Select.propTypes = {
+  options: PropTypes.array.isRequired,
+  onChange: PropTypes.func.isRequired,
+  name: PropTypes.string.isRequired,
+  placeholder: PropTypes.string.isRequired,
+  value: PropTypes.string.isRequired,
 };
 
 export default Select;
