2dbadf3045abbb1b31ae2a0863f972dc4243e007
Jhosua Pachon <jhosuapachon@gmail.com>
2021-03-08 23:11:07 -0500

    added PropTypes to Button

--- src/Components/Button/index.js
+++ src/Components/Button/index.js
@@ -1,11 +1,19 @@
 import React from "react";
 import { ButtonForm } from "./styles";
+import PropTypes from "prop-types";
+
 const Button = ({ type, textButton, fill }) => {
   return (
     <ButtonForm type={type} fill={fill}>
       {textButton}
     </ButtonForm>
   );
+};
+
+Button.propTypes = {
+  type: PropTypes.string.isRequired,
+  textButton: PropTypes.string.isRequired,
+  fill: PropTypes.string.isRequired,
 };
 
 export default Button;
