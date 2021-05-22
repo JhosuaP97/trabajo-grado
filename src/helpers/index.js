11e258c3141774a8bb7309b25ece5b69955c077d
Jhosua Pachon <jhosuapachon@gmail.com>
2021-05-22 15:56:12 -0500

    added a new function

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,13 +1,32 @@
-// function handleChangeMultiSelect(value, e) {
-//   if (e.action === "select-option" && e.option.value === "*") {
-//     setSelectedOptions(options);
-//   } else if (e.action === "deselect-option" && e.option.value === "*") {
-//     setSelectedOptions([]);
-//   } else if (e.action === "deselect-option") {
-//     setSelectedOptions(value.filter((o) => o.value !== "*"));
-//   } else if (value.length === options.length - 1) {
-//     setSelectedOptions(options);
-//   } else {
-//     setSelectedOptions(value);
-//   }
-// }
+export function handleChangeMultiSelect(
+  value,
+  e,
+  options,
+  componentName,
+  setState,
+  state
+) {
+  if (e.name === componentName) {
+    if (e.action === "select-option" && e.option.value === "*") {
+      setState({
+        ...state,
+        [e.name]: options,
+      });
+    } else if (e.action === "clear") {
+      setState({
+        ...state,
+        [e.name]: [],
+      });
+    } else if (e.action === "remove-value") {
+      setState({
+        ...state,
+        [e.name]: value.filter((option) => option.value !== "*"),
+      });
+    } else {
+      setState({
+        ...state,
+        [e.name]: value,
+      });
+    }
+  }
+}
