fec1b933d87cb8127fc682d29ef99f614d603f21
Jhosua Pachon <jhosuapachon@gmail.com>
2021-05-22 15:48:32 -0500

    update styles form component

--- src/Components/Form/styles.js
+++ src/Components/Form/styles.js
@@ -2,9 +2,6 @@ import styled from "styled-components";
 
 export const FormStyle = styled.form`
   margin: 4rem;
-  /* display: flex;
-  flex-direction: column;
-  justify-content: space-around; */
 `;
 
 export const Title = styled.h2`
@@ -16,14 +13,9 @@ export const Title = styled.h2`
 export const Row = styled.section`
   display: flex;
   flex-wrap: wrap;
+  align-items: center;
   gap: 21px;
-  margin: 0.2rem 0;
-  width: 100%;
-`;
-
-export const Field = styled.div`
-  width: ${({ select }) => (select ? "19rem" : "10rem")};
-  margin: 0.3rem 0;
+  margin: 1.25rem 0;
 `;
 
 export const WrapperRadio = styled.div`
@@ -34,6 +26,8 @@ export const WrapperRadio = styled.div`
 `;
 
 export const ButtonActions = styled.div`
-  display: flex;
   margin: 4.625rem 0 0 0;
+  & > button {
+    margin: 0 0.3rem 0 0;
+  }
 `;
