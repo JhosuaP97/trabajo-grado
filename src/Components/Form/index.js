5b7f3d5d839e79dd0829a4aadc4e4419daf738f2
Jhosua Pachon <jhosuapachon@gmail.com>
2021-05-22 15:48:05 -0500

    update form component

--- src/Components/Form/index.js
+++ src/Components/Form/index.js
@@ -1,28 +1,24 @@
 import React, { Fragment, useState } from "react";
 import TextField from "Components/TextField";
-import {
-  Title,
-  Row,
-  Field,
-  FormStyle,
-  WrapperRadio,
-  ButtonActions,
-} from "./styles";
+import { Title, Row, FormStyle, WrapperRadio, ButtonActions } from "./styles";
 
 //Components
-import Select from "Components/Select";
+import SelectStyle from "Components/Select";
 import Button from "Components/Button";
 import MultiSelectAll from "Components/MultiSelectAll";
 import RadioButton from "Components/RadioButton";
 import TextArea from "Components/TextArea";
 import ProductGroups from "Components/ProductGroups";
+import Practice from "Components/Practice";
 
+import { handleChangeMultiSelect } from "helpers";
 //Data
 import {
   optionsParticipantes,
   optionsModulos,
   optionsProducto,
   optionsNumGrupos,
+  optionsGraficos,
 } from "constants/index";
 
 import useGroup from "hooks/useGroup";
@@ -39,18 +35,11 @@ const initialStateForm = {
 
 const Form = () => {
   const [field, setFields] = useState(initialStateForm);
-  const {
-    groups,
-    AddNewGroup,
-    handleChangeGroups,
-    handleProductGroups,
-  } = useGroup();
+  const { groups, AddNewGroup, handleChangeGroups, handleProductGroups } =
+    useGroup();
 
-  const {
-    individual,
-    handleChangeIndividual,
-    handleProductIndividual,
-  } = useIndividual();
+  const { individual, handleChangeIndividual, handleProductIndividual } =
+    useIndividual();
 
   /* Función que recibe un evento, este evento evalua si el name que recibe 
   termina en individual, lo añade al estado individual sino lo añade
@@ -62,33 +51,6 @@ const Form = () => {
       [e.target.name]: e.target.value,
     });
   };
-
-  function handleChangeMultiSelect(value, e) {
-    if (e.name === "participantes") {
-      if (e.action === "select-option" && e.option.value === "*") {
-        setFields({
-          ...field,
-          [e.name]: optionsParticipantes,
-        });
-      } else if (e.action === "clear") {
-        setFields({
-          ...field,
-          [e.name]: [],
-        });
-      } else if (e.action === "remove-value") {
-        setFields({
-          ...field,
-          [e.name]: value.filter((option) => option.value !== "*"),
-        });
-      } else {
-        setFields({
-          ...field,
-          [e.name]: value,
-        });
-      }
-    }
-  }
-
   /* Función que recibe un evento que con el valor que le llega añade un nuevo grupo
   al estado groups  */
 
@@ -114,26 +76,78 @@ const Form = () => {
     if (practice === "grupo") {
       return (
         <>
-          <Title>Crear grupo</Title>
-          <Row>
-            <Field>
-              <Select
-                name="numGrupo"
-                placeholder="Nº de grupos"
-                options={optionsNumGrupos}
-                value={field.numGrupo || ""}
-                onChange={handleAddNewGroup}
-              />
-            </Field>
-          </Row>
-          {groups && (
-            <ProductGroups
-              groups={groups}
-              optionsProducto={optionsProducto}
-              onChange={handleChangeGroups}
-              products={handleProductGroups}
-              integrantes={field.participantes}
-            />
+          {field.modulo !== "" ? (
+            <>
+              <Title>Crear grupo</Title>
+              <Row>
+                <SelectStyle
+                  name="numGrupo"
+                  width={"7rem"}
+                  placeholder="Nº de grupos"
+                  options={optionsNumGrupos}
+                  value={field.numGrupo || ""}
+                  onChange={handleAddNewGroup}
+                />
+                {field.modulo === "Corte 2" && (
+                  <MultiSelectAll
+                    name="graficos"
+                    options={[
+                      { label: "Todo los graficos", value: "*" },
+                      ...optionsGraficos,
+                    ]}
+                    value={field.graficos || ""}
+                    placeholder="Seleccionar gráficos"
+                    onChange={(value, e) =>
+                      handleChangeMultiSelect(
+                        value,
+                        e,
+                        optionsGraficos,
+                        "graficos",
+                        setFields,
+                        field
+                      )
+                    }
+                  />
+                )}
+              </Row>
+              {field.modulo === "Corte 3" && (
+                <Row>
+                  <WrapperRadio>
+                    <p>Tipo de muestreo:</p>
+                    <RadioButton
+                      name="tipoMuestreo"
+                      id="variables"
+                      value="variable"
+                      text="Variables"
+                      onChange={handleChange}
+                      checked={field.tipoMuestreo === "variable"}
+                    />
+                    <RadioButton
+                      name="tipoMuestreo"
+                      id="atributos"
+                      value="atributo"
+                      text="Atributos"
+                      onChange={handleChange}
+                      checked={field.tipoMuestreo === "atributo"}
+                    />
+                  </WrapperRadio>
+                </Row>
+              )}
+
+              {groups.length > 0 && (
+                <ProductGroups
+                  groups={groups}
+                  optionsProducto={optionsProducto}
+                  onChange={handleChangeGroups}
+                  products={handleProductGroups}
+                  integrantes={field.participantes}
+                  modulo={field.modulo}
+                  tipoMuestreo={field.tipoMuestreo}
+                />
+              )}
+            </>
+          ) : (
+            <p>Debes elegir un modulo</p>
           )}
         </>
       );
@@ -144,24 +158,24 @@ const Form = () => {
         <>
           <Title>Práctica individual</Title>
           <Row>
-            <Field>
-              <Select
-                name="productoIndividual"
-                placeholder="Seleccionar producto"
-                options={optionsProducto}
-                value={individual.productoIndividual || ""}
-                onChange={handleChangeIndividual}
-              />
-            </Field>
-            <Field>
-              <TextField
-                type="number"
-                name="unidadesIndividual"
-                placeholder="Unidades"
-                value={individual.unidadesIndividual || ""}
-                onChange={handleChangeIndividual}
-              />
-            </Field>
+            <SelectStyle
+              width={"9rem"}
+              name="productoIndividual"
+              placeholder="Seleccionar producto"
+              options={optionsProducto}
+              value={individual.productoIndividual || ""}
+              onChange={handleChangeIndividual}
+            />
+
+            <TextField
+              type="number"
+              name="unidadesIndividual"
+              width={"7.625rem"}
+              placeholder="Unidades"
+              value={individual.unidadesIndividual || ""}
+              onChange={handleChangeIndividual}
+            />
+
             {individual.productoIndividual &&
               handleProductIndividual(
                 individual.productoIndividual,
@@ -178,84 +192,88 @@ const Form = () => {
     <FormStyle onSubmit={handleSubmit} noValidate>
       <Title>Configurar nueva práctica</Title>
       <Row>
-        <Field>
-          <TextField
-            type="text"
-            name="practica"
-            placeholder="Nombre de la práctica"
-            value={field.practica || ""}
-            onChange={handleChange}
-          />
-        </Field>
-        <Field>
-          <Select
-            name="modulo"
-            placeholder="Módulo"
-            options={optionsModulos}
-            value={field.modulo || ""}
-            onChange={handleChange}
-          />
-        </Field>
-        <Field select>
-          <MultiSelectAll
-            name="participantes"
-            options={[
-              { label: "Todo el grupo", value: "*" },
-              ...optionsParticipantes,
-            ]}
-            value={field.participantes || ""}
-            placeholder="Participantes"
-            onChange={handleChangeMultiSelect}
-          />
-        </Field>
+        <TextField
+          type="text"
+          name="practica"
+          placeholder="Nombre de la práctica"
+          value={field.practica || ""}
+          onChange={handleChange}
+        />
+        <SelectStyle
+          name="modulo"
+          width={"7rem"}
+          placeholder="Módulo"
+          options={optionsModulos}
+          value={field.modulo || ""}
+          onChange={handleChange}
+        />
+        <MultiSelectAll
+          name="participantes"
+          options={[
+            { label: "Todo el grupo", value: "*" },
+            ...optionsParticipantes,
+          ]}
+          value={field.participantes || ""}
+          placeholder="Participantes"
+          onChange={(value, e) =>
+            handleChangeMultiSelect(
+              value,
+              e,
+              optionsParticipantes,
+              "participantes",
+              setFields,
+              field
+            )
+          }
+        />
       </Row>
       {/* Tipo de práctica */}
       <Row>
-        <Field>
-          <WrapperRadio>
-            <p>Tipo de práctica:</p>
-            <RadioButton
-              name="tipoPractica"
-              id="grupos"
-              value="grupo"
-              text="Por grupos"
-              onChange={handleChange}
-              checked={field.tipoPractica === "grupo"}
-            />
-            <RadioButton
-              name="tipoPractica"
-              id="individual"
-              value="individual"
-              text="Individual"
-              onChange={handleChange}
-              checked={field.tipoPractica === "individual"}
-            />
-          </WrapperRadio>
-        </Field>
+        <WrapperRadio>
+          <p>Tipo de práctica:</p>
+          <RadioButton
+            name="tipoPractica"
+            id="grupos"
+            value="grupo"
+            text="Por grupos"
+            onChange={handleChange}
+            checked={field.tipoPractica === "grupo"}
+          />
+          <RadioButton
+            name="tipoPractica"
+            id="individual"
+            value="individual"
+            text="Individual"
+            onChange={handleChange}
+            checked={field.tipoPractica === "individual"}
+          />
+        </WrapperRadio>
       </Row>
       {/* Descripción de la práctica */}
       <Row>
-        <Field>
-          <TextArea
-            name="descripcion"
-            placeholder="Descripción"
-            cols="88"
-            rows="7"
-            value={field.descripcion || ""}
-            onChange={handleChange}
-          />
-        </Field>
+        <TextArea
+          name="descripcion"
+          placeholder="Descripción"
+          value={field.descripcion || ""}
+          onChange={handleChange}
+        />
       </Row>
       {/* Crear grupo o práctica individual */}
       {field.tipoPractica && generatePractice(field.tipoPractica)}
+      {/* {field.tipoPractica && (
+        <Practice
+          practice={field.tipoPractica}
+          field={field}
+          setFields={setFields}
+          groups={groups}
+          handleChange={handleChange}
+        />
+      )} */}
+
       <Row>
         <ButtonActions>
-          <Field>
-            <Button type="button" textButton="Cancelar" fill="false" />
-          </Field>
-          <Field>
-            <Button type="submit" textButton="Publicar" fill="true" />
-          </Field>
+          <Button type="button" textButton="Cancelar" fill="false" />
+          <Button type="submit" textButton="Publicar" fill="true" />
         </ButtonActions>
       </Row>
     </FormStyle>
