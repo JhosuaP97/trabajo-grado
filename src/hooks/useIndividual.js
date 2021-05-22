82e4377e97f929a7958218bc5d9b626a815fef37
Jhosua Pachon <jhosuapachon@gmail.com>
2021-05-22 15:57:00 -0500

    updated hook

--- src/hooks/useIndividual.js
+++ src/hooks/useIndividual.js
@@ -1,14 +1,17 @@
 import React, { Fragment, useState } from "react";
-import { Field } from "Components/Form/styles";
 import Select from "Components/Select";
 import TextField from "Components/TextField";
+import MultiSelectAll from "Components/MultiSelectAll";
 
 const initialStateIndividual = {
   productoIndividual: "",
   unidadesIndividual: 0,
   contIndividual: 0,
   toleranciaIndividual: 0,
+  atributos: [],
 };
+
+const SIZE_FIELD = "7rem";
 
 const useIndividual = () => {
   const [individual, setIndividual] = useState(initialStateIndividual);
@@ -20,6 +23,10 @@ const useIndividual = () => {
     });
   };
 
+  function handleChangeMultiSelectIndividual(value, e) {
+    console.log(value, e);
+  }
+
   /* Función que recibe 3 parametros, productName hace referencia al valor que es escogido por
   el usuario por medio del select, field se usa para actualizar estado de ese componente
   el último se refiere al arreglo de los productos */
@@ -30,24 +37,35 @@ const useIndividual = () => {
       .map((productSelected, i) => {
         return (
           <Fragment key={i}>
-            <Field>
-              <Select
-                name="contIndividual"
-                placeholder={productSelected.placeholder}
-                options={productSelected.contOptions}
-                value={field.contIndividual || ""}
-                onChange={handleChangeIndividual}
-              />
-            </Field>
-            <Field>
-              <TextField
-                type="number"
-                name="toleranciaIndividual"
-                placeholder="Tolerancia"
-                value={field.toleranciaIndividual || ""}
-                onChange={handleChangeIndividual}
-              />
-            </Field>
+            <Select
+              name="contIndividual"
+              width={SIZE_FIELD}
+              placeholder={productSelected.placeholder}
+              options={productSelected.contOptions}
+              value={field.contIndividual || ""}
+              onChange={handleChangeIndividual}
+            />
+
+            <TextField
+              type="number"
+              width={SIZE_FIELD}
+              name="toleranciaIndividual"
+              placeholder="Tolerancia"
+              value={field.toleranciaIndividual || ""}
+              onChange={handleChangeIndividual}
+            />
+
+            <MultiSelectAll
+              name="atributosIndividual"
+              options={productSelected.attributes}
+              getOptionLabel={(option) => option.label}
+              getOptionValue={(option) => option.id}
+              value={field.atributos}
+              placeholder="Atributos"
+              onChange={(value, e) =>
+                handleChangeMultiSelectIndividual(value, e)
+              }
+            />
           </Fragment>
         );
       });
