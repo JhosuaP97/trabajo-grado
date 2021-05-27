88d869170ec303852813eb2871c5ecc72ca172fe
Jhosua Pachon <jhosuapachon@gmail.com>
2021-05-27 14:45:50 -0500

    update PracticeIndividual component

--- src/Components/Practice/PracticeIndividual.js
+++ src/Components/Practice/PracticeIndividual.js
@@ -1,13 +1,30 @@
 import React, { useContext } from "react";
+
+// Styles
 import { Row, Title, WrapperRadio } from "Components/Form/styles";
+
+// Data
+import {
+  optionsProducto,
+  optionsGraficos,
+  CORTE2,
+  CORTE3,
+  CORTE1,
+} from "constants/index";
+
+// Context
+import IndividualContext from "Context/Individual/IndividualContext";
+
+// hooks
+import useIndividual from "hooks/useIndividual";
+
+// helpers
+import { handleChangeMultiSelect } from "helpers";
+
+// Components
 import SelectStyle from "Components/Select";
 import TextField from "Components/TextField";
-import useIndividual from "hooks/useIndividual";
-import { optionsProducto, optionsGraficos } from "constants/index";
-
-import IndividualContext from "Context/Individual/IndividualContext";
 import MultiSelectAll from "Components/MultiSelectAll";
-import { handleChangeMultiSelect } from "helpers";
 import RadioButton from "Components/RadioButton";
 
 const PracticeIndividual = ({ field, setFields, handleChange }) => {
@@ -19,59 +36,62 @@ const PracticeIndividual = ({ field, setFields, handleChange }) => {
     handleChange(e);
     handleChangeIndividual(e);
   };
+
+  const corte2 = () => (
+    <Row>
+      <MultiSelectAll
+        name="graficos"
+        options={[
+          { label: "Todo los graficos", value: "*" },
+          ...optionsGraficos,
+        ]}
+        value={field.graficos || ""}
+        placeholder="Seleccionar gráficos"
+        onChange={(value, e) =>
+          handleChangeMultiSelect({
+            value: value,
+            event: e,
+            options: optionsGraficos,
+            componentName: "graficos",
+            setState: setFields,
+            state: field,
+          })
+        }
+      />
+    </Row>
+  );
+
+  const corte3 = () => (
+    <Row>
+      <WrapperRadio>
+        <p>Tipo de muestreo:</p>
+        <RadioButton
+          name="tipoMuestreo"
+          id="variables"
+          value="variable"
+          text="Variables"
+          onChange={handleChange}
+          checked={field.tipoMuestreo === "variable"}
+        />
+        <RadioButton
+          name="tipoMuestreo"
+          id="atributos"
+          value="atributo"
+          text="Atributos"
+          onChange={handleChange}
+          checked={field.tipoMuestreo === "atributo"}
+        />
+      </WrapperRadio>
+    </Row>
+  );
+
   return (
     <>
       {field.modulo !== "" ? (
         <>
           <Title>Práctica individual</Title>
-          {field.modulo === "Corte 2" && (
-            <Row>
-              <MultiSelectAll
-                name="graficos"
-                options={[
-                  { label: "Todo los graficos", value: "*" },
-                  ...optionsGraficos,
-                ]}
-                value={field.graficos || ""}
-                placeholder="Seleccionar gráficos"
-                onChange={(value, e) =>
-                  handleChangeMultiSelect({
-                    value: value,
-                    event: e,
-                    options: optionsGraficos,
-                    componentName: "graficos",
-                    setState: setFields,
-                    state: field,
-                  })
-                }
-              />
-            </Row>
-          )}
-          {field.modulo === "Corte 3" && (
-            <Row>
-              <Row>
-                <WrapperRadio>
-                  <p>Tipo de muestreo:</p>
-                  <RadioButton
-                    name="tipoMuestreo"
-                    id="variables"
-                    value="variable"
-                    text="Variables"
-                    onChange={handleChange}
-                    checked={field.tipoMuestreo === "variable"}
-                  />
-                  <RadioButton
-                    name="tipoMuestreo"
-                    id="atributos"
-                    value="atributo"
-                    text="Atributos"
-                    onChange={handleChange}
-                    checked={field.tipoMuestreo === "atributo"}
-                  />
-                </WrapperRadio>
-              </Row>
-            </Row>
-          )}
+          {field.modulo === CORTE2 && corte2()}
+          {field.modulo === CORTE3 && corte3()}
           <Row>
             <SelectStyle
               width={"9rem"}
@@ -81,14 +101,16 @@ const PracticeIndividual = ({ field, setFields, handleChange }) => {
               value={field.productoIndividual || ""}
               onChange={handleOnChange}
             />
-            <TextField
-              type="number"
-              name="unidadesIndividual"
-              width={"7.625rem"}
-              placeholder="Unidades"
-              value={individual.unidadesIndividual || ""}
-              onChange={handleOnChange}
-            />
+            {field.modulo === CORTE1 && (
+              <TextField
+                type="number"
+                name="unidadesIndividual"
+                width={"7.625rem"}
+                placeholder="Unidades"
+                value={individual.unidadesIndividual || ""}
+                onChange={handleOnChange}
+              />
+            )}
 
             {individual &&
               handleProductIndividual(
