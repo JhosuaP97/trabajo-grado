f5f2d790726634c750dc3f9c807e3284f02ee616
Jhosua Pachon <jhosuapachon@gmail.com>
2021-06-02 17:23:23 -0500

    Added field state

---
+++ src/Context/Field/FieldState.js
@@ -0,0 +1,61 @@
+import React, { useReducer } from "react";
+import {
+  CHANGE_FIELD,
+  CHANGE_FIELD_MULTI_SELECT,
+  CHANGE_FIELD_MULTI_SELECT_ALL,
+} from "types/index";
+import FieldContext from "./FieldContext";
+import FieldReducer from "./FieldReducer";
+
+const FieldState = ({ children }) => {
+  const initialState = {
+    field: {
+      practica: "",
+      modulo: "",
+      participantes: [],
+      tipoPractica: "",
+      descripcion: "",
+      numGrupo: "",
+      tipoMuestreo: "",
+    },
+  };
+
+  const [state, dispatch] = useReducer(FieldReducer, initialState);
+
+  const handleChangeField = (e) => {
+    const { name, value } = e.target;
+    dispatch({
+      type: CHANGE_FIELD,
+      payload: { name, value },
+    });
+  };
+
+  const handleChangeMultiSelectField = (value, e, options) => {
+    const { name, action, option } = e;
+    if (action === "select-option" && option.value === "*") {
+      dispatch({
+        type: CHANGE_FIELD_MULTI_SELECT_ALL,
+        payload: { name, options },
+      });
+    } else {
+      dispatch({
+        type: CHANGE_FIELD_MULTI_SELECT,
+        payload: { value, name },
+      });
+    }
+  };
+
+  return (
+    <FieldContext.Provider
+      value={{
+        field: state.field,
+        handleChangeField,
+        handleChangeMultiSelectField,
+      }}
+    >
+      {children}
+    </FieldContext.Provider>
+  );
+};
+
+export default FieldState;
