ef93c204b8bdfb4aea5911c2f3e1f5690db19c92
Jhosua Pachon <jhosuapachon@gmail.com>
2021-08-03 17:22:17 -0500

    add PageMessage Component

--- src/Components/PageMessage/index.js
+++ src/Components/PageMessage/index.js
@@ -1,21 +1,21 @@
 import React from "react";
 // Components
-import PageMessageTableModule1 from "./PageMessageTableModule1";
-import PageMessageConstant from "./PageMessageConstant";
-import PageMessageVariableProductConstant from "./PageMessageVariableProductConstant";
-import PageStartModule3 from "./PageStartModule3";
-const SummaryPage = () => {
-  const pageString = "page1";
+import SummaryModule1 from "./SummaryModule1";
+import NiceWorkMessage from "./NiceWorkMessage";
+import DoingGreatMessage from "./DoingGreatMessage";
+import ConditionsContainer from "./ConditionsContainer";
+const PageMessage = () => {
+  const pageString = "pageModule1";
 
   const SELECTEDPAGE = {
-    page1: <PageMessageTableModule1 />,
-    page2: <PageMessageConstant />,
-    page3: <PageMessageVariableProductConstant />,
-    page4: <PageStartModule3 />,
+    pageModule1: <SummaryModule1 />,
+    pageModule2A: <NiceWorkMessage />,
+    pageModule2B: <DoingGreatMessage />,
+    pageModule3: <ConditionsContainer />,
   };
 
-  const page = SELECTEDPAGE[pageString] || <PageMessageTableModule1 />;
+  const page = SELECTEDPAGE[pageString] || <SummaryModule1 />;
   return <>{page}</>;
 };
 
-export default SummaryPage;
+export default PageMessage;
