283a07ef9642853900ad87e1b6e3305160fed1f4
Jhosua Pachon <jhosuapachon@gmail.com>
2021-08-03 17:30:26 -0500

    update Dashboards wth some errors

--- src/Components/StudentDashboard/index.js
+++ src/Components/StudentDashboard/index.js
@@ -11,13 +11,6 @@ import {
   EXTRA_INFO_SHOW,
 } from "constants/index";
 
-// Models
-import Refresco from "models/Refresco.glb";
-import RefrescoTapaFloja from "models/Refresco_tapa_floja.glb";
-import RefrescoTapaFlojaEnvaseSucio from "models/Refresco_tapa_floja_envase_sucio.glb";
-import RefrescoEtiquetaSueltaEnvaseSucioTapaFloja from "models/Refresco_etiqueta_suelta_envase_sucio_tapa_floja.glb";
-import RefrescoEnvaseSucio from "models/Refresco_envase_sucio.glb";
-import RefrescoDefectuoso from "models/Refresco_defectuoso.glb";
 import StudentExtraInfo from "Components/StudentExtraInfo";
 import ProgressBar from "Components/ProgressBar";
 
@@ -30,105 +23,78 @@ import {
   ProductsDisplay,
   MainStudent,
 } from "./styles";
+import PageMessage from "Components/PageMessage";
 
-const info = [
-  { name: "contenido", value: "355 ml+-5" },
-  { name: "cantidad gas", value: "255 ml+-5" },
-  { name: "tapa", value: "fija" },
-  { name: "etiqueta", value: "fija" },
-  { name: "Envase", value: "limpio" },
-  { name: "Textos", value: "Ilegibles" },
-];
-
-const arrProductos = [
-  {
-    id: 0,
-    src: Refresco,
-    contenido: "355 ml",
-    cantidad_gas: "15%",
-  },
-  {
-    id: 1,
-    src: RefrescoTapaFloja,
-    contenido: "350 ml",
-    cantidad_gas: "10%",
-  },
-  {
-    id: 2,
-    src: RefrescoTapaFlojaEnvaseSucio,
-    contenido: "360 ml",
-    cantidad_gas: "20%",
-  },
-  {
-    id: 3,
-    src: RefrescoEtiquetaSueltaEnvaseSucioTapaFloja,
-    contenido: "355 ml",
-    cantidad_gas: "15%",
-  },
-  {
-    id: 4,
-    src: RefrescoEnvaseSucio,
-    contenido: "356 ml",
-    cantidad_gas: "15%",
-  },
-  {
-    id: 5,
-    src: RefrescoDefectuoso,
-    contenido: "355 ml",
-    cantidad_gas: "18%",
-  },
-];
+import useStudent from "hooks/useStudent";
+import useReviewSubgroup from "hooks/useReviewSubgroup";
+import useProductsState from "hooks/useProductsState";
 
 const StudentDashboard = () => {
+  // Hooks
+  const { products, modulo, selectedSubgroup } = useStudent();
+  const { counterSubgroup, isCounterEmpty, numberOfReviewed } =
+    useReviewSubgroup();
+  const {
+    reviewed,
+    checked,
+    rejected,
+    handleReview,
+    handleChecked,
+    handleRejected,
+  } = useProductsState();
   // States
-  const [reviewed, setReviewed] = useState([0]);
-  const [rejected, setRejected] = useState([]);
-  const [checked, setChecked] = useState([]);
+  const validSubgroup = selectedSubgroup !== null && selectedSubgroup;
 
-  // Counts
+  const [selectedIdSubgroup, setSelectedIdSubgroup] = useState(0);
+
   const countReviewed = reviewed.length;
   const countRejected = rejected.length;
   const countChecked = checked.length;
 
-  const modulo = CORTE1;
-
-  const selectedInfotoShow = EXTRA_INFO_SHOW[modulo];
-  const selectedSteps = STEPS_BY_MODULE[modulo];
-  const selectedProductType = TYPEOF_GRAPHICS_PRODUCT["variable"];
+  // const selectedInfotoShow = EXTRA_INFO_SHOW[modulo];
+  // const selectedSteps = STEPS_BY_MODULE[modulo];
+  // const selectedProductType = TYPEOF_GRAPHICS_PRODUCT["constant"];
 
   const isModulo3 = (value) => (modulo === CORTE3 ? value : null);
 
   return (
     <BackgrounContainer>
       <MainStudent>
-        <ConfigPractice>
+        {/* <ConfigPractice>
           <ProgressBar steps={selectedSteps} />
           {modulo !== CORTE3 && (
             <StudentExtraInfo infotoShow={selectedInfotoShow} />
           )}
-        </ConfigPractice>
+        </ConfigPractice> */}
         <StudentData>
+          {/* <PageMessage /> */}
           <ProductsDisplay>
             <ImageSlider
-              currentModule={modulo}
-              images={arrProductos}
+              counterSubgroup={counterSubgroup}
               reviewed={reviewed}
-              setReviewed={setReviewed}
               rejected={isModulo3(rejected)}
               checked={isModulo3(checked)}
-              setRejected={isModulo3(setRejected)}
-              setChecked={isModulo3(setChecked)}
+              handleReview={handleReview}
+              handleChecked={handleChecked}
+              handleRejected={handleRejected}
             />
           </ProductsDisplay>
           <Info>
             <StudentInfo
-              currentModule={modulo}
-              countReviewed={countReviewed}
-              totalReviewed={arrProductos.length}
+              numberOfReviewed={numberOfReviewed}
+              selectedIdSubgroup={selectedIdSubgroup}
+              setSelectedIdSubgroup={setSelectedIdSubgroup}
+              handleReview={handleReview}
+              countReviewed={
+                modulo === CORTE2 ? isCounterEmpty() : countReviewed
+              }
+              totalReviewed={
+                modulo === CORTE2
+                  ? selectedSubgroup?.grupos?.length
+                  : products.length
+              }
               rejected={countRejected}
               checked={countChecked}
-              features={info}
-              typeOfProductGraphics={selectedProductType}
             />
           </Info>
         </StudentData>
