195f8bc4513fdbb9342798f344743a4d1f57a161
Jhosua Pachon <jhosuapachon@gmail.com>
2021-08-03 17:20:30 -0500

    add field to show the date

--- src/Components/Banner/index.js
+++ src/Components/Banner/index.js
@@ -5,12 +5,14 @@ const Banner = () => {
     <BannerContainer>
       <Content>
         <h1>Curso 51</h1>
-        <h3>Siete erramientas</h3>
+        <h3>Siete herramientas</h3>
         <p>Modulo: Corte 1</p>
         <small>
           Descripción: Observa cada uno de los productos y anota sus
           características en el formato.
         </small>
+        <br />
+        <small>Fecha de publicación: 1/08/2021</small>
       </Content>
     </BannerContainer>
   );
