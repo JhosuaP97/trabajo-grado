43e2b1c804b2699264185fd1182e04a5e035cc3d
Jhosua Pachon <jhosuapachon@gmail.com>
2021-08-03 17:29:04 -0500

    add StudentState

--- src/App.js
+++ src/App.js
@@ -5,22 +5,26 @@ import Form from "./Components/Form";
 import TeacherDashboard from "Components/Teacher/TeacherDashboard";
 import StudentDashboard from "Components/StudentDashboard";
 
+import StudentState from "context/Student/StudentState";
+
 function App() {
   return (
-    <BrowserRouter>
-      <GlobalStyles />
-      <Switch>
-        <Route path="/" exact>
-          <Form />
-        </Route>
-        <Route path="/teacher" exact>
-          <TeacherDashboard />
-        </Route>
-        <Route path="/student" exact>
-          <StudentDashboard />
-        </Route>
-      </Switch>
-    </BrowserRouter>
+    <StudentState>
+      <BrowserRouter>
+        <GlobalStyles />
+        <Switch>
+          <Route path="/" exact>
+            <Form />
+          </Route>
+          <Route path="/teacher" exact>
+            <TeacherDashboard />
+          </Route>
+          <Route path="/student" exact>
+            <StudentDashboard />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </StudentState>
   );
 }
 
