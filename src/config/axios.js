16e2b061a0eb8369be62af4728b99d5da58f04b3
Jhosua Pachon <jhosuapachon@gmail.com>
2021-09-22 00:39:42 -0500

    update

--- src/config/axios.js
+++ src/config/axios.js
@@ -2,6 +2,9 @@ import axios from "axios";
 
 const axiosClient = axios.create({
   baseURL: process.env.REACT_APP_BACKEND_URL,
+  headers: {
+    "Access-Control-Allow-Origin": "*",
+  },
 });
 
 export default axiosClient;
