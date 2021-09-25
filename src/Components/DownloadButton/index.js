78bdb7c94ea2402eba52485023277d05d1535be4
Jhosua Pachon <jhosuapachon@gmail.com>
2021-09-25 16:36:30 -0500

    add files to download button

--- src/Components/DownloadButton/index.js
+++ src/Components/DownloadButton/index.js
@@ -1,9 +1,16 @@
 import React from "react";
 import { StyleDownloadButton } from "./styles";
+import tableConstantControlGraphic from "Files/Tabla de constantes para graficos de control.pdf";
+import tableAnsi from "Files/tabla muestreo ANSI.rar";
+
+const files = {
+  table: tableConstantControlGraphic,
+  ansi: tableAnsi,
+};
 
 const DownloadButton = ({ file, children }) => {
   return (
-    <StyleDownloadButton to={file} target="_blank" download>
+    <StyleDownloadButton to={files[file]} target="_blank" download>
       {children}
     </StyleDownloadButton>
   );
